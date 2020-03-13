ec32b7f1884172c0a1758feb82a19669cce27799
Andrea Villegas <andrea@villegas.com>
2020-03-12 22:56:25 -0400

    new styling added

---
+++ master/nav_header.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+<header>
+    <div class="header-navigation">
+        <div class="header-nav-content">
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="nav-link">
+                    <ul class="list-inline">
+                        <li class="list-inline-item">
+                            <a class="nav-link text-light" href="#">Home</a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a class="nav-link text-light" href="#">Events</a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a class="nav-link text-light" href="#">Create Events</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="logo">MAD<span>EVENTS</span></div>
+
+                <div class="login-btn">
+                    <button type="button" class="btn-general">Sign In</button>
+                    <button type="button" class="btn-general">Sign Up</button>
+                </div>
+            </div>
+        </div>
+</header>
+
+
