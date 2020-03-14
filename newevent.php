f2594917db1fe67403a1d2bc7a5c5ff8b4500de5
Andrea Villegas <andrea@villegas.com>
2020-03-13 23:12:47 -0400

    Event Dashboard Created

--- newevent.php
+++ newevent.php
@@ -4,6 +4,7 @@
 
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>New Event</title>
     <link rel="stylesheet" type="text/css" href="css/myevent.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
